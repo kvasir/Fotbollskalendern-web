18cab0380f318ea514a84486161d88d3631e183e
MartinSandstrom <Martin.sandstrom@avanade.com>
2014-09-01 14:48:30 +0200

    Set html5Mode to true

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,77 +14,78 @@ angular
   ])
 	.value('MatchInfo', [
 		{
-			id: 1,
-			homeTeam: 'Jps-dreamteam',
-			awayTeam: 'Martins heroes',
-			time: '21:00',
-			league: 'Golden division'
+		    id: 1,
+		    homeTeam: 'Jps-dreamteam',
+		    awayTeam: 'Martins heroes',
+		    time: '21:00',
+		    league: 'Golden division'
 		},
 		{
-			id: 2,
-			homeTeam: 'Marcus pojkar',
-			awayTeam: 'Jerkers xD-team',
-			time: '17:00',
-			league: 'Fikaligan'
+		    id: 2,
+		    homeTeam: 'Marcus pojkar',
+		    awayTeam: 'Jerkers xD-team',
+		    time: '17:00',
+		    league: 'Fikaligan'
 		},
 		{
-			id: 3,
-			homeTeam: 'The craigesters',
-			awayTeam: 'Core service',
-			time: '19:00',
-			league: 'Superettan'
+		    id: 3,
+		    homeTeam: 'The craigesters',
+		    awayTeam: 'Core service',
+		    time: '19:00',
+		    league: 'Superettan'
 		},
 		{
-			id: 4,
-			homeTeam: 'Barcelona',
-			awayTeam: 'Real Madrid',
-			time: '06:00',
-			league: 'Superettan'
+		    id: 4,
+		    homeTeam: 'Barcelona',
+		    awayTeam: 'Real Madrid',
+		    time: '06:00',
+		    league: 'Superettan'
 		},
 		{
-			id: 5,
-			homeTeam: 'Podlaget',
-			awayTeam: 'Arsenal',
-			time: '09:00',
-			league: 'Superettan'
+		    id: 5,
+		    homeTeam: 'Podlaget',
+		    awayTeam: 'Arsenal',
+		    time: '09:00',
+		    league: 'Superettan'
 		},
 		{
-			id: 6,
-			homeTeam: 'Dream Team',
-			awayTeam: 'Complexity',
-			time: '19:00',
-			league: 'Golden division'
+		    id: 6,
+		    homeTeam: 'Dream Team',
+		    awayTeam: 'Complexity',
+		    time: '19:00',
+		    league: 'Golden division'
 		},
 		{
-			id: 7,
-			homeTeam: 'Djurgården',
-			awayTeam: 'Lycksele IF',
-			time: '19:00',
-			league: 'Golden division'
+		    id: 7,
+		    homeTeam: 'Djurgården',
+		    awayTeam: 'Lycksele IF',
+		    time: '19:00',
+		    league: 'Golden division'
 		},
 		{
-			id: 8,
-			homeTeam: 'Tanterna plan 5',
-			awayTeam: 'Supportcenter',
-			time: '19:00',
-			league: 'Fikaligan'
+		    id: 8,
+		    homeTeam: 'Tanterna plan 5',
+		    awayTeam: 'Supportcenter',
+		    time: '19:00',
+		    league: 'Fikaligan'
 		}
 	])
-  .config(function ($routeProvider) {
-    $routeProvider
+  .config(function ($routeProvider, $locationProvider) {
+      $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
       })
-      .when('/about', { 
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/about', {
+          templateUrl: 'views/about.html',
+          controller: 'AboutCtrl'
       })
 			.when('/match', {
-				templateUrl: 'views/match.html',
-				controller: 'MatchCtrl'
+			    templateUrl: 'views/match.html',
+			    controller: 'MatchCtrl'
 			})
       .otherwise({
-        redirectTo: '/'
+          redirectTo: '/'
       });
+      $locationProvider.html5Mode(true);
   });
