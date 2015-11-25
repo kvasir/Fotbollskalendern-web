cdc98a51965e63d3c8345c5f08231ba65f8206df
MartinSandstrom <Martin.sandstrom@avanade.com>
2015-11-25 09:50:18 +0100

    Replacing old logic with new conditions from the api

--- app/scripts/services/teamService.js
+++ app/scripts/services/teamService.js
@@ -4,19 +4,18 @@ angular.module('fotbollskalendernWebApp')
     .factory('TeamService', function ($http, $q) {
 		var apiKey = '2ecca4360cd746d5a4808ba2b8e1fa96';
         return {
-            getGamesFromTeam: function (teamName) {
-                var games = [];
+            getTeamInformation: function(url){
                 var gameResult = $q.defer();
-                all.then(function (results) {
-                    results.forEach(function (result) {
-                        var allGames = result.data;
-                        allGames.forEach(function(game){
-                          if (game.homeTeamName === teamName || game.awayTeamName === teamName) {
-                              games.push(game);
-                          }
-                      });
-                     });
-                    gameResult.resolve(games);
+                $http.get(url, { headers: { 'X-Auth-Token': apiKey }}).then(function(result){
+                    gameResult.resolve(result.data);
+                });
+
+                return gameResult.promise;
+            },
+            getGamesFromUrl: function(url){
+                var gameResult = $q.defer();
+                $http.get(url, { headers: { 'X-Auth-Token': apiKey }}).then(function(result){
+                    gameResult.resolve(result.data);
                 });
 
                 return gameResult.promise;
