4cf32340dbb791cac1512dfaf905986a332eef67
MartinSandstrom <Martin.sandstrom@avanade.com>
2015-12-01 10:02:16 +0100

    Adding new constants for tables
    Thsi should be removed/rethought later

--- app/scripts/constants/leagues.js
+++ app/scripts/constants/leagues.js
@@ -1,22 +1,27 @@
 angular.module('fotbollskalendernWebApp').constant('Leagues', [
         {
             name: 'Premier league',
-            url: 'http://api.football-data.org/v1/soccerseasons/398/fixtures'
+            url: 'http://api.football-data.org/v1/soccerseasons/398/fixtures',
+            table: 'http://api.football-data.org/v1/soccerseasons/398/leagueTable'
         },
         {
             name: 'La liga',
-            url: 'http://api.football-data.org/v1/soccerseasons/399/fixtures'
+            url: 'http://api.football-data.org/v1/soccerseasons/399/fixtures',
+            table: 'http://api.football-data.org/v1/soccerseasons/399/leagueTable'
         },
         {
             name: 'Bundesliga',
-            url: 'http://api.football-data.org/v1/soccerseasons/394/fixtures'
+            url: 'http://api.football-data.org/v1/soccerseasons/394/fixtures',
+            table: 'http://api.football-data.org/v1/soccerseasons/394/leagueTable'
         },
         {
             name: 'Serie A',
-            url: 'http://api.football-data.org/v1/soccerseasons/401/fixtures'
+            url: 'http://api.football-data.org/v1/soccerseasons/401/fixtures',
+            table: 'http://api.football-data.org/v1/soccerseasons/401/leagueTable'
         },
         {
             name: 'Franskaligan',
-            url: 'http://api.football-data.org/v1/soccerseasons/396/fixtures'
+            url: 'http://api.football-data.org/v1/soccerseasons/396/fixtures',
+            table: 'http://api.football-data.org/v1/soccerseasons/396/leagueTable'
         }
     ]);
