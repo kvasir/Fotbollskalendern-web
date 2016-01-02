845e40534f34098d219426d4a90519e923f92823
Martin Sandström <maartiin_91@hotmail.com>
2016-01-02 16:32:53 +0100

    Making shortcut a directive and to use Leagues constants.

--- app/scripts/directives/shortcuts.js
+++ app/scripts/directives/shortcuts.js
@@ -1,9 +1,18 @@
 'use strict';
 
 angular.module('fotbollskalendernWebApp')
-	.directive('shortcuts', function () {
-		return {
-			templateUrl: 'scripts/directives/shortcuts.html',
-			restrict: 'E'
-		};
-	});
+.directive('shortcuts', function ($location, Leagues) {
+	return {
+		templateUrl: 'scripts/directives/shortcuts.html',
+		restrict: 'E',
+		link: function ($scope) {
+			$scope.leagues = Leagues;
+			$scope.viewLeague = function (league) {
+				var obj = Leagues.filter(function (obj) {
+					return obj.name === league;
+				})[0];
+				$location.path('league').search('url', obj.url);
+			};
+		}
+	};
+});
