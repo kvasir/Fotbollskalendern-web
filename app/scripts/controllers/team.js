685d44f1bf14028db55f7c288557a96a1c5acac9
Martin Sandström <maartiin_91@hotmail.com>
2016-01-03 15:54:53 +0100

    Fixed indentation

--- app/scripts/controllers/team.js
+++ app/scripts/controllers/team.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('fotbollskalendernWebApp')
-    .controller('TeamCtrl', function ($scope, $location, DataService) {
-        var url = $location.search().url;
-        $scope.games = {};
-        var teamInformation = {};
+.controller('TeamCtrl', function ($scope, $location, DataService) {
+	var url = $location.search().url;
+	$scope.games = {};
+	var teamInformation = {};
 
-        DataService.getDataFromUrl(url).then(function(data){
-            teamInformation = data;
-            DataService.getDataFromUrl(teamInformation._links.fixtures.href).then(function(games){
-                $scope.games = games.fixtures;
-                $scope.teamInformation = teamInformation;
-            });
-        });
+	DataService.getDataFromUrl(url).then(function (data) {
+		teamInformation = data;
+		DataService.getDataFromUrl(teamInformation._links.fixtures.href).then(function (games) {
+			$scope.games = games.fixtures;
+			$scope.teamInformation = teamInformation;
+		});
+	});
 
-        $scope.matchInfo = function (game) {
-            $location.path('match').search('url', game._links.self.href);
-        };
-    });
+	$scope.matchInfo = function (game) {
+		$location.path('match').search('url', game._links.self.href);
+	};
+});
