ad67262bb34e34c6d3319b94c2c0373cbd52978b
Martin Sandström <maartiin_91@hotmail.com>
2016-01-04 15:06:44 +0100

    Adding functionality to save teams to favourites.

--- app/scripts/controllers/team.js
+++ app/scripts/controllers/team.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('fotbollskalendernWebApp')
-.controller('TeamCtrl', function ($scope, $location, DataService) {
+.controller('TeamCtrl', function ($scope, $location, DataService, FavouritesService) {
 	var url = $location.search().url;
+	$scope.url = url;
 	$scope.games = {};
 	var teamInformation = {};
 
@@ -13,6 +14,10 @@ angular.module('fotbollskalendernWebApp')
 			$scope.teamInformation = teamInformation;
 		});
 	});
+
+	$scope.addToFavourites = function (teamName, url) {
+		FavouritesService.toggleFavourite(teamName, url);
+	};
 
 	$scope.matchInfo = function (game) {
 		$location.path('match').search('url', game._links.self.href);
