84e4878fec5015f06ace74e8eda75c31181e8661
Martin Sandström <maartiin_91@hotmail.com>
2016-08-06 14:28:44 +0200

    adding new ids for leagues

--- app/scripts/constants/leagues.js
+++ app/scripts/constants/leagues.js
@@ -1,22 +1,22 @@
 angular.module('fotbollskalendernWebApp').constant('Leagues', [
 	{
 		name: 'Premier league',
-		url: 'http://api.football-data.org/v1/soccerseasons/398/'
+		url: 'http://api.football-data.org/v1/soccerseasons/426/'
 	},
 	{
 		name: 'La liga',
-		url: 'http://api.football-data.org/v1/soccerseasons/399/'
+		url: 'http://api.football-data.org/v1/soccerseasons/436/'
 	},
 	{
 		name: 'Bundesliga',
-		url: 'http://api.football-data.org/v1/soccerseasons/394/'
+		url: 'http://api.football-data.org/v1/soccerseasons/430/'
 	},
 	{
 		name: 'Serie A',
-		url: 'http://api.football-data.org/v1/soccerseasons/401/'
+		url: 'http://api.football-data.org/v1/soccerseasons/438/'
 	},
 	{
 		name: 'Franskaligan',
-		url: 'http://api.football-data.org/v1/soccerseasons/396/'
+		url: 'http://api.football-data.org/v1/soccerseasons/434/'
 	}
 ]);
