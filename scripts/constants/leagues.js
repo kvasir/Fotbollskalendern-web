5b21052d9913ffaa61976a19fd999ac00cf2a618
Martin Sandström <maartiin_91@hotmail.com>
2016-08-16 14:52:08 +0200

    Update 2016-08-16T12:52:06.437Z

---
+++ scripts/constants/leagues.js
@@ -0,0 +1,22 @@
+angular.module('fotbollskalendernWebApp').constant('Leagues', [
+	{
+		name: 'Premier league',
+		url: 'http://api.football-data.org/v1/soccerseasons/426/'
+	},
+	{
+		name: 'La liga',
+		url: 'http://api.football-data.org/v1/soccerseasons/436/'
+	},
+	{
+		name: 'Bundesliga',
+		url: 'http://api.football-data.org/v1/soccerseasons/430/'
+	},
+	{
+		name: 'Serie A',
+		url: 'http://api.football-data.org/v1/soccerseasons/438/'
+	},
+	{
+		name: 'Franskaligan',
+		url: 'http://api.football-data.org/v1/soccerseasons/434/'
+	}
+]);
