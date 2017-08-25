27ff31e17f1298ec95d3812de93a7f6636c4d3f9
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2017-08-25 11:12:51 +0200

    Add new season

--- app/scripts/constants/leagues.js
+++ app/scripts/constants/leagues.js
@@ -1,22 +1,22 @@
 angular.module('fotbollskalendernWebApp').constant('Leagues', [
 	{
 		name: 'Premier league',
-		url: 'http://api.football-data.org/v1/soccerseasons/426/'
+		url: 'http://api.football-data.org/v1/soccerseasons/445/'
 	},
 	{
 		name: 'La liga',
-		url: 'http://api.football-data.org/v1/soccerseasons/436/'
+		url: 'http://api.football-data.org/v1/soccerseasons/455/'
 	},
 	{
 		name: 'Bundesliga',
-		url: 'http://api.football-data.org/v1/soccerseasons/430/'
+		url: 'http://api.football-data.org/v1/soccerseasons/452/'
 	},
 	{
 		name: 'Serie A',
-		url: 'http://api.football-data.org/v1/soccerseasons/438/'
+		url: 'http://api.football-data.org/v1/soccerseasons/456/'
 	},
 	{
 		name: 'Franskaligan',
-		url: 'http://api.football-data.org/v1/soccerseasons/434/'
+		url: 'http://api.football-data.org/v1/soccerseasons/450/'
 	}
 ]);
